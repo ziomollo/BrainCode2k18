a612b120c307ee34f52559dea9c8950291806e89
Bartosz Ruminski <bartoshx@gmail.com>
2018-04-20 21:24:25 +0200

    POJO 2

---
+++ Server/Bifrost/src/main/java/com/heimdall/bifrost/BifrostApplication.java
@@ -0,0 +1,12 @@
+package com.heimdall.bifrost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BifrostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BifrostApplication.class, args);
+    }
+}
---
+++ Server/Bifrost/src/test/java/com/heimdall/bifrost/BifrostApplicationTests.java
@@ -0,0 +1,16 @@
+package com.heimdall.bifrost;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BifrostApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
