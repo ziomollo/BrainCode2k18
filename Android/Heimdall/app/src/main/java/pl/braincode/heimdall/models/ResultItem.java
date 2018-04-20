b977e65c97d882513645cae7eeb07f7c368c0455
Bartosz Ruminski <bartoshx@gmail.com>
2018-04-20 21:22:13 +0200

    POJO 2

---
+++ Android/Heimdall/app/src/main/java/pl/braincode/heimdall/models/ResultItem.java
@@ -0,0 +1,12 @@
+package pl.braincode.heimdall.models;
+
+public class ResultItem {
+
+    public String title;
+    public String url;
+
+    public ResultItem(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+}
