a773a1348f30783f44ae3ee02372ce695ef998df
unknown <jaroslaw.koltonowski@gmail.com>
2018-04-21 14:31:20 +0200

    Refactor

--- Server/Bifrost/src/main/java/com/heimdall/bifrost/allegroapi/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "https://webapi.allegro.pl/service.php", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.heimdall.bifrost.allegroapi;
